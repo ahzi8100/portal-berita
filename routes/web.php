2eb0b137c7ea7f4367624799b93c8c7617f3079b
Ahmad Fauzi <uzif8100@students.amikom.ac.id>
2023-09-01 14:32:08 +0700

    Done CRUD Categories

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,8 @@
 <?php
 
+use App\Http\Controllers\CategoriesController;
 use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\TestController;
-use SebastianBergmann\CodeCoverage\Report\Html\Dashboard;
 
 /*
 |--------------------------------------------------------------------------
@@ -32,3 +31,17 @@ use SebastianBergmann\CodeCoverage\Report\Html\Dashboard;
 
 Route::get('/', [DashboardController::class, 'home'])->name('home');
 Route::get('/about', [DashboardController::class, 'about'])->name('about');
+
+// Create Data Category
+Route::get('/categories/categories', [CategoriesController::class, 'create'])->name('category.create');
+Route::post('/categories', [CategoriesController::class, 'store'])->name('category.store');
+
+// Read Data Category
+Route::get('/categories', [CategoriesController::class, 'index'])->name('category.index');
+
+// Update Data Categor
+Route::get('/categories/{id}/edit', [CategoriesController::class, 'edit'])->name('category.edit');
+Route::put('/categories/{id}', [CategoriesController::class, 'update'])->name('category.update');
+
+// Delete Data Category
+Route::delete('/categories/{id}', [CategoriesController::class, 'destroy'])->name('category.destroy');
